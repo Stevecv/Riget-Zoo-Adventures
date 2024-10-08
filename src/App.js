cce7c939874175ada5e7ce9964bdd9531d0e810c
40204509 <40204509@hruc.ac.uk>
2024-10-08 12:58:37 +0100

    Animal Cards

--- src/App.js
+++ src/App.js
@@ -4,8 +4,45 @@ import lion from './/images/lion.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faClock, faHippo, faPerson, faInfoCircle} from "@fortawesome/free-solid-svg-icons";
 import Footer from "./Footer";
+import {BrowserRouter, Link, Router} from 'react-router-dom';
+
+function ButtonLink({ to, children }) {
+    return <link to={to}><button>{children}</button></link>;
+}
 
 function App() {
+    const animals = [
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        },
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        },
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        },
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        },
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        },
+        {
+            title: "Lion",
+            image: lion,
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dapibus eros in turpis sollicitudin, eu congue elit dignissim. Nunc non finibus augue. Aliquam ultrices leo lectus, condimentum vestibulum justo vestibulum eget.",
+        }
+    ]
   return (
     <div className="App">
         <header className="App-header font-plain">
@@ -36,26 +73,38 @@ function App() {
             </div>
 
             <div className="flex justify-center items-center w-full pb-10 gap-5">
-                <div className="bg-primary text-white text-background w-96 p-5 rounded-xl hover:scale-105 duration-200">
-                    <div className="flex justify-center items-center">
+                <BrowserRouter>
+                    <Link to="/accessability" className="bg-primary text-white flex  justify-center items-center text-background w-96 p-5 rounded-xl hover:scale-105 duration-200">
                         <FontAwesomeIcon icon={faClock} className="fa-xl pr-5"/>
                         <div className="text-xl">Open from 10am</div>
-                    </div>
-                </div>
+                    </Link>
 
-                <div className="bg-primary text-white text-background w-96 p-5 rounded-xl hover:scale-105 duration-200">
-                    <div className="flex justify-center items-center">
+                    <Link to="/accessability" className="bg-primary text-white flex  justify-center items-center text-background w-96 p-5 rounded-xl hover:scale-105 duration-200">
                         <FontAwesomeIcon icon={faPerson} className="fa-xl pr-5"/>
                         <div className="text-xl">Accessibility</div>
-                    </div>
-                </div>
+                    </Link>
 
-                <div className="bg-primary text-white w-96 p-5 rounded-xl hover:scale-105 duration-200">
-                    <div className="flex justify-center items-center">
+                    <Link to="/accessability" className="bg-primary text-white flex  justify-center items-center text-background w-96 p-5 rounded-xl hover:scale-105 duration-200">
                         <FontAwesomeIcon icon={faInfoCircle} className="fa-xl pr-5"/>
                         <div className="text-xl">Information</div>
+                    </Link>
+                </BrowserRouter>
+            </div>
+
+            <h2 id="our-animals" className="text-7xl p-7">
+                Our Animals
+            </h2>
+
+            <div className="w-full grid grid-cols-1 md:grid-cols-3 pl-[10%] pr-[10%] place-items-center">
+                {animals.map((animal, index) => (
+                    <div className="w-[80%] rounded-xl shadow-xl bg-background-secondary text-white hover:scale-105 hover:shadow-2xl duration-200 m-5">
+                        <img src={animal.image} className="rounded-t-xl w-full"/>
+
+                        <h2 className="text-5xl p-4">{animal.title}</h2>
+
+                        <div className="p-3 text-xl">{animal.description}</div>
                     </div>
-                </div>
+                    ))}
             </div>
 
             <Footer/>
