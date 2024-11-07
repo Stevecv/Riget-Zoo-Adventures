b496e2026e4be710cfa6b48656db654a11baee7a
40204509 <40204509@hruc.ac.uk>
2024-11-07 16:54:03 +0000

    Changes

--- src/HotelBooking.js
+++ src/HotelBooking.js
@@ -5,6 +5,7 @@ import {useNavigate} from "react-router-dom";
 import UserProfile from "./UserProfile";
 import Cookies from "js-cookie";
 import InputField from "./components/InputField";
+import HotelRoom from "./components/HotelRoom";
 
 export default function HotelBooking() {
     const [email, setEmail] = useState('');
@@ -98,41 +99,8 @@ export default function HotelBooking() {
             <div className="bg-background-400 w-screen h-screen flex flex-col">
                 <Navbar />
 
-                <div className="flex-grow flex items-center justify-center ">
-                    <div className="bg-background-100 w-96 h-fit rounded-xl shadow-xl p-5 text-2xl">
-                        <div className="text-3xl w-full text-center">
-                            Login
-                        </div>
 
-                        <form onSubmit={handleSubmit}>
-                            <InputField id="email" label="Email Address" placeholder="someone@example.com" type="email"
-                                        value={email} onChange={(e) => setEmail(e.target.value)}/>
-                            <InputField
-                                id="password"
-                                label="Password"
-                                placeholder=""
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                className={error ? 'border-red-500' : ''}
-                            />
-
-                            <div className="text-sm pl-2">
-                                Don't have an account? <a href="/register" className="text-[#0000ee]">Register</a>
-                            </div>
-
-                            <div className="text-lg pl-2 text-error">
-                                {error}
-                            </div>
-
-                            <button
-                                className="bg-primary text-center rounded-xl shadow-xl p-2 m-2 hover:bg-primary-600 duration-200 w-full"
-                                onClick={handleSubmit}>
-                                Login
-                            </button>
-                        </form>
-                    </div>
-                </div>
+                <HotelRoom />
             </div>
             <Footer/>
         </div>
--- src/components/HotelRoom.js
+++ src/components/HotelRoom.js
@@ -1,10 +1,11 @@
 import Cookies from "js-cookie";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
+import Navbar from "../Navbar";
 
 export default function HotelRoom() {
     return (
-        <div>
+        <div className="w-[80%] md:w-full">
 
         </div>
     )
